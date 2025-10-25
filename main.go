22a9345f0c9eff15e7c60aa95d699bf6a4d26212
vinnunama <vinnunama@gmail.com>
2025-10-25 08:09:01 +0530

    Implement input handling and processing in main function

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"os"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex >")
+	for scanner.Scan() {
+		input := scanner.Text()
+		words := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", words[0])
+		fmt.Print("Pokedex >")
+	}
+
+}
+
+func cleanInput(text string) []string {
+	//The purpose of this function will be to split the user's input into "words" based on whitespace. 
+	// It should also lowercase the input and trim any leading or trailing whitespace.
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+	
+	words := strings.Fields(text)
+	return words
 }
